1fa733e5d53da4f5ec9fb4ba97f7ec692182ae21
Vitaly Khan IDEA <swng69@yandex.ru>
2019-02-02 15:58:05 +0800

    4th commit

--- src/main/java/net/my/Person.java
+++ src/main/java/net/my/Person.java
@@ -1,4 +1,22 @@
 package net.my;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
